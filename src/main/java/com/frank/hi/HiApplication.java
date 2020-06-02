fe2f7b95f9d90d0c90fe41974e4d2710062c6048
Liu Frank <frank.2.liu@nokia-sbell.com>
2020-06-02 08:17:58 +0300

    Add code from start.spring.io for start

--- src/main/java/com/frank/hi/HiApplication.java
+++ src/main/java/com/frank/hi/HiApplication.java
@@ -1,12 +1,13 @@
-package com.manning.readinglist;
+package com.frank.hi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ReadingListApplication {
+public class HiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ReadingListApplication.class, args);
+		SpringApplication.run(HiApplication.class, args);
 	}
+
 }
